5c7457c900c95eb70084973b72b46ad3d1f57bda
success-99 <101646238+success-99@users.noreply.github.com>
2022-07-01 00:59:30 +0500

    adadad

--- 14 dars.py
+++ 14 dars.py
@@ -1,2 +1,53 @@
-cars={"model":"BMW", "narx":"40000$", "yili":"2021", "davlat":"Germaniya"}
-print(cars["model"])
+# cars={"model":"BMW", "narx":"40000$", "yili":"2021", "davlat":"Germaniya"}
+# print(f"{cars['model'].lower()}")
+# cars['rangi']='oq'
+# cars["rangi"]="qora"
+# del cars["yili"]
+# print(cars)
+
+poytaxtlar={
+"uzbekiston":"tashkent",
+"russia":"moscov",
+"italiya":"rim",
+"angliya":"london",
+"germaniya":"berlin",
+"saudiya arabistoni":"ar-riyod"
+}
+# /////////////////////////////////////
+
+# poytaxt = poytaxtlar.get("saudiya arabistoni", "Bu poytaxt lug'atda yo'q")
+# print(poytaxt.title())
+
+# print([poytaxtlar.items()])
+
+# ////////////////////////////////////////
+
+# for k, q in poytaxtlar.items():
+#     print(f" \nDavlat > {k.title()},  Poytaxt > {q.title()}\n")
+# ////////////////////////////////////////
+# print(poytaxtlar.keys())
+
+# for m in poytaxtlar.keys():
+#     print(f"{m.title() } ")
+
+
+# for m in poytaxtlar.values():
+#     print(f"   {m.title() }") 
+# ///////////////////////////////////////////////
+
+b_poytaxtlar=["tashkent", "qohira", "vashington", "ar-riyod"]
+bor_p=[]
+yoq_p=[]
+for t in b_poytaxtlar:
+    if t in poytaxtlar.values():
+        bor_p.append(t.title())
+        # print(f" Bor {t}")
+    else:
+        yoq_p.append(t.title())
+        # print(f"Yo'q {t}")
+print("Bileti mavjud bo'lgan Poytaxtlar: ")
+for i in bor_p:
+    print(i)
+print("Bileti tugagan Poytaxtlar: ")
+for g in yoq_p:
+    print(g)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
- 
+i mavjud bo'lgan Poytaxtlar: ")
+# for i in bor_p:
+#     print(t)
+# print("Bileti tugagan Poytaxtlar: ")
+# for i in yoq_p:
+#     print(t)
